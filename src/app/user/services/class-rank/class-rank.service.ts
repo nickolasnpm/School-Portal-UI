99c601c9add26435ba6581e52b568fa7f24c344a
Nickolas Pius Matu <nickolasnpm.work@gmail.com>
2025-05-05 22:44:56 +0800

    update rank management component

--- src/app/user/services/class-rank/class-rank.service.ts
+++ src/app/user/services/class-rank/class-rank.service.ts
@@ -3,8 +3,9 @@ import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../../../environments/environment.development';
-import { classRankResponse } from '../../models/ClassRank';
+import { classRankRequest, classRankResponse } from '../../models/ClassRank';
 import { HttpService } from '../../../template/services/http/http.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +15,8 @@ export class ClassRankService {
 
   constructor(
     private _httpClient: HttpClient,
-    private _httpService: HttpService
+    private _httpService: HttpService,
+    private _authService: AuthService
   ) {}
 
   getAll(isActive: boolean): Observable<classRankResponse[]> {
@@ -24,10 +26,52 @@ export class ClassRankService {
     params = params.append('isActive', isActive);
 
     return this._httpClient
-      .get<classRankResponse[]>(apiUrl, { params })
+      .get<classRankResponse[]>(apiUrl, {
+        headers: this._authService.getHeaders(),
+        params,
+      })
       .pipe(
         catchError((error) =>
           this._httpService.handleError('GetAllClassRanks', error)
+        )
+      );
+  }
+
+  create(request: classRankRequest): Observable<boolean> {
+    const apiUrl = `${this.controllerUrl}`;
+    return this._httpClient
+      .post<boolean>(`${apiUrl}`, request, {
+        headers: this._authService.getHeaders(),
+      })
+      .pipe(
+        catchError((error) =>
+          this._httpService.handleError('Create Rank', error)
+        )
+      );
+  }
+
+  update(id: string, request: classRankRequest): Observable<boolean> {
+    const apiUrl = `${this.controllerUrl}/${id}`;
+    return this._httpClient
+      .put<boolean>(`${apiUrl}`, request, {
+        headers: this._authService.getHeaders(),
+      })
+      .pipe(
+        catchError((error) =>
+          this._httpService.handleError('Update Rank', error)
+        )
+      );
+  }
+
+  delete(id: string): Observable<boolean> {
+    const apiUrl = `${this.controllerUrl}/${id}`;
+    return this._httpClient
+      .delete<boolean>(`${apiUrl}`, {
+        headers: this._authService.getHeaders(),
+      })
+      .pipe(
+        catchError((error) =>
+          this._httpService.handleError('Delete Rank', error)
         )
       );
   }
